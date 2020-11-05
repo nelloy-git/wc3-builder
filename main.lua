1366ed0512ae3758da693278707a1f6e7efd4fac
Nelloy <nelloy93@yandex.ru>
2020-11-05 16:16:38 +0300

    work

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local sep = package.config:sub(1,1)
-local this_file_path = debug.getinfo(2, "S").source:sub(2)
+local this_file_path = debug.getinfo(1, "S").source:sub(2)
 local this_dir_path = this_file_path:sub(1, this_file_path:match('^.*()'..sep))
 
 -- Add builder dir to packages list
